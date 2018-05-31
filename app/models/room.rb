c751d8801f7fe3432432a4d92a80b65996dc4904
capaisa <capaisa12@gmail.com>
2018-05-30 20:34:49 -0500

    Se le agrega las validaciones y se modifica el formulario, para que muestre los errores q se generan por las validaciones

--- app/models/room.rb
+++ app/models/room.rb
@@ -13,4 +13,10 @@
 #
 
 class Room < ActiveRecord::Base
+  # Validad que todos los campos sean obliatorios
+  validates :title, :description, :beds, :guests, :image_url, presence: true
+  # validad que el campo description no tenga mas de 400 caracteres
+  validates :description, length: {maximum: 400}
+  # El campor beds y guests
+  validates :beds, :guests, numericality: {only_integer: true}
 end
